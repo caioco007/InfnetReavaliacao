76afd357ce21d976fc4090c0d695aea81f3fc3f5
Caio Gomes <caio.gomes@casion.com.br>
2023-10-14 15:28:11 -0300

    XUNIT implementation

---
+++ InfnetReavaliacao.UnitTestes/Application/Services/AuthorServiceTests.cs
@@ -0,0 +1,126 @@
+﻿using InfnetReavaliacao.Application.InputModels;
+using InfnetReavaliacao.Application.Services.Implementations;
+using InfnetReavaliacao.Core.Entities;
+using InfnetReavaliacao.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfnetReavaliacao.UnitTestes.Application.Services
+{
+    public class AuthorServiceTests
+    {
+        [Fact]
+        public void Create_Method_Returns_AuthorId()
+        {
+            // Arrange
+            var authorRepository = new Mock<IAuthorRepository>();
+            var authorService = new AuthorService(authorRepository.Object);
+            var inputModel = new NewAuthorInputModel
+            {
+                FullName = "Sample FullName",
+                BirthDate = new DateTime(2000, 10, 12),
+                Country = "Suécia"
+            };
+            var generatedId = 1;
+
+            authorRepository.Setup(repo => repo.GeneratedId()).Returns(generatedId);
+
+            // Act
+            var result = authorService.Create(inputModel);
+
+            // Assert
+            Assert.Equal(generatedId, result);
+            authorRepository.Verify(repo => repo.Create(It.IsAny<Author>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetAll_Method_Returns_NonDeleted_Authors()
+        {
+            // Arrange
+            var authorRepository = new Mock<IAuthorRepository>();
+            var authorService = new AuthorService(authorRepository.Object);
+            var authors = new List<Author>
+            {
+                new Author(1, "Author 1", new DateTime(1960, 12, 14), "Suíça"),
+                new Author(2, "Author 2", new DateTime(1950, 11, 13), "França"),
+                new Author(3, "Author 3", new DateTime(1940, 10, 12), "Nigéria"),
+            };
+            authorRepository.Setup(repo => repo.GetAll()).Returns(authors);
+
+            // Act
+            var result = authorService.GetAll();
+
+            // Assert
+            Assert.Equal(3, result.Count);
+        }
+
+        [Fact]
+        public void GetById_Method_Returns_AuthorViewModel()
+        {
+            // Arrange
+            var authorRepository = new Mock<IAuthorRepository>();
+            var authorService = new AuthorService(authorRepository.Object);
+            var authorId = 1;
+            var author = new Author(authorId, "Sample FullName", new DateTime(2000, 12, 12), "China");
+            authorRepository.Setup(repo => repo.GetById(authorId)).Returns(author);
+
+            // Act
+            var result = authorService.GetById(authorId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(authorId, result.Id);
+            authorRepository.Verify(repo => repo.GetById(authorId), Times.Once);
+        }
+
+        [Fact]
+        public void Delete_Method_Calls_Cancel_OnAuthor()
+        {
+            // Arrange
+            var authorRepository = new Mock<IAuthorRepository>();
+            var authorService = new AuthorService(authorRepository.Object);
+            var authorId = 1;
+            var author = new Author(authorId, "Sample FullName", new DateTime(2000, 10, 12), "Argentina");
+            authorRepository.Setup(repo => repo.GetById(authorId)).Returns(author);
+
+            // Act
+            authorService.Delete(authorId);
+
+            // Assert
+            authorRepository.Verify(repo => repo.GetById(authorId), Times.Once);
+            Assert.True(author.IsDeleted);
+        }
+
+        [Fact]
+        public void Update_Method_Calls_Update_OnAuthor()
+        {
+            // Arrange
+            var authorRepository = new Mock<IAuthorRepository>();
+            var authorService = new AuthorService(authorRepository.Object);
+            var authorId = 1;
+            var author = new Author(authorId, "Sample FullName", new DateTime(2000, 10, 12), "Uruguai");
+            var inputModel = new UpdateAuthorInputModel
+            {
+                Id = authorId,
+                FullName = "New FullName",
+                BirthDate= new DateTime(1998, 03, 17),
+                Country = "Japão"
+            };
+
+            authorRepository.Setup(repo => repo.GetById(authorId)).Returns(author);
+
+            // Act
+            authorService.Update(inputModel);
+
+            // Assert
+            authorRepository.Verify(repo => repo.GetById(authorId), Times.Once);
+            Assert.Equal(inputModel.FullName, author.FullName);
+            Assert.Equal(inputModel.BirthDate, author.BirthDate);
+            Assert.Equal(inputModel.Country, author.Country);
+        }
+    }
+}
---
+++ InfnetReavaliacao.UnitTestes/Application/Services/BookServiceTests.cs
@@ -0,0 +1,125 @@
+﻿using InfnetReavaliacao.Application.InputModels;
+using InfnetReavaliacao.Application.Services.Implementations;
+using InfnetReavaliacao.Core.Entities;
+using InfnetReavaliacao.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace InfnetReavaliacao.UnitTestes.Application.Services
+{
+    public class BookServiceTests
+    {
+        [Fact]
+        public void Create_Method_Returns_BookId()
+        {
+            // Arrange
+            var bookRepository = new Mock<IBookRepository>();
+            var bookService = new BookService(bookRepository.Object);
+            var inputModel = new NewBookInputModel
+            {
+                Title = "Sample Title",
+                Description = "Sample Description",
+                IdAuthor = 1
+            };
+            var generatedId = 1;
+
+            bookRepository.Setup(repo => repo.GeneratedId()).Returns(generatedId);
+
+            // Act
+            var result = bookService.Create(inputModel);
+
+            // Assert
+            Assert.Equal(generatedId, result);
+            bookRepository.Verify(repo => repo.Create(It.IsAny<Book>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetAll_Method_Returns_NonDeleted_Books()
+        {
+            // Arrange
+            var bookRepository = new Mock<IBookRepository>();
+            var bookService = new BookService(bookRepository.Object);
+            var books = new List<Book>
+            {
+                new Book(1, "Book 1", "Description 1", 1),
+                new Book(2, "Book 2", "Description 2", 2),
+                new Book(3, "Book 3", "Description 3", 1),
+            };
+            bookRepository.Setup(repo => repo.GetAll()).Returns(books);
+
+            // Act
+            var result = bookService.GetAll();
+
+            // Assert
+            Assert.Equal(3, result.Count);
+        }
+
+        [Fact]
+        public void GetById_Method_Returns_BookViewModel()
+        {
+            // Arrange
+            var bookRepository = new Mock<IBookRepository>();
+            var bookService = new BookService(bookRepository.Object);
+            var bookId = 1;
+            var book = new Book(bookId, "Sample Title", "Sample Description", 2);
+            bookRepository.Setup(repo => repo.GetById(bookId)).Returns(book);
+
+            // Act
+            var result = bookService.GetById(bookId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(bookId, result.Id);
+            bookRepository.Verify(repo => repo.GetById(bookId), Times.Once);
+        }
+
+        [Fact]
+        public void Delete_Method_Calls_Cancel_OnBook()
+        {
+            // Arrange
+            var bookRepository = new Mock<IBookRepository>();
+            var bookService = new BookService(bookRepository.Object);
+            var bookId = 1;
+            var book = new Book(bookId, "Sample Title", "Sample Description", 2);
+            bookRepository.Setup(repo => repo.GetById(bookId)).Returns(book);
+
+            // Act
+            bookService.Delete(bookId);
+
+            // Assert
+            bookRepository.Verify(repo => repo.GetById(bookId), Times.Once);
+            Assert.True(book.IsDeleted);
+        }
+
+        [Fact]
+        public void Update_Method_Calls_Update_OnBook()
+        {
+            // Arrange
+            var bookRepository = new Mock<IBookRepository>();
+            var bookService = new BookService(bookRepository.Object);
+            var bookId = 1;
+            var book = new Book(bookId, "Sample Title", "Sample Description", 3);
+            var inputModel = new UpdateBookInputModel
+            {
+                Id = bookId,
+                Title = "New Title",
+                Description = "New Description"
+            };
+
+            bookRepository.Setup(repo => repo.GetById(bookId)).Returns(book);
+
+            // Act
+            bookService.Update(inputModel);
+
+            // Assert
+            bookRepository.Verify(repo => repo.GetById(bookId), Times.Once);
+            Assert.Equal(inputModel.Title, book.Title);
+            Assert.Equal(inputModel.Description, book.Description);
+        }
+    }
+}
